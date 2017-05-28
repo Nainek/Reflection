e984f78576a7add840eb8ab6198207cdc60a0886
Taras Nainek <nainek95@mail.ua>
2017-05-28 18:21:27 +0300

    Added base components to Window

--- src/main/java/ua/training/model/windows/Window.java
+++ src/main/java/ua/training/model/windows/Window.java
@@ -1,7 +1,64 @@
 package ua.training.model.windows;
 
-/**
- * Created by Nainek on 28.05.2017.
- */
+
+
+
 public class Window {
+
+    private int leftCoordinate;
+    private int rightCoordinate;
+    private int topCoordinate;
+    private int botoomCoordinate;
+
+    private String color;
+
+
+
+    public Window(int leftCoordinate, int rightCoordinate, int topCoordinate, int botoomCoordinate, String color) {
+        this.leftCoordinate = leftCoordinate;
+        this.rightCoordinate = rightCoordinate;
+        this.topCoordinate = topCoordinate;
+        this.botoomCoordinate = botoomCoordinate;
+        this.color = color;
+    }
+
+    public int getLeftCoordinate() {
+        return leftCoordinate;
+    }
+
+    public void setLeftCoordinate(int leftCoordinate) {
+        this.leftCoordinate = leftCoordinate;
+    }
+
+    public int getRightCoordinate() {
+        return rightCoordinate;
+    }
+
+    public void setRightCoordinate(int rightCoordinate) {
+        this.rightCoordinate = rightCoordinate;
+    }
+
+    public int getTopCoordinate() {
+        return topCoordinate;
+    }
+
+    public void setTopCoordinate(int topCoordinate) {
+        this.topCoordinate = topCoordinate;
+    }
+
+    public int getBotoomCoordinate() {
+        return botoomCoordinate;
+    }
+
+    public void setBotoomCoordinate(int botoomCoordinate) {
+        this.botoomCoordinate = botoomCoordinate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
